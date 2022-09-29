93e7c5586ee6e4de93f25bba7f215cde628be005
Thiago Manzi <thiago_manzi@hotmail.com>
2022-09-29 17:01:33 -0300

    parte 1

---
+++ Carro.java
@@ -0,0 +1,31 @@
+public class Carro{
+    
+    //variaveis de instancia
+    private String modelo;
+    private String placa;
+
+    //setters
+    public void setModelo(String m){
+        modelo = m;
+    }
+    public void setPlaca(String p){
+        placa = p;
+    }
+
+    //getters
+    //setters
+    public String getModelo(){
+        return modelo;
+    }
+    public String getPlaca(){
+        return placa;
+    }
+
+    //metodos de instancia
+    public void acelerarCarro(){
+        System.out.println("Acelerando o carro");
+    }
+    public void capotarCarro(){
+        System.out.printf("Capotei o %s\n", modelo);
+    }
+}
